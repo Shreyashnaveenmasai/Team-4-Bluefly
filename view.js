9d5307e5f4118624eefc927cb82c5887de29905c
Arun-ez <arunshaw433@gmail.com>
2023-01-09 14:33:00 +0530

    Add to cart functionality added for product viewfinder and minor css changes made

--- view.js
+++ view.js
@@ -1,11 +1,11 @@
 
 let obj = JSON.parse(localStorage.getItem("viewObj"));
-let index = JSON.parse(localStorage.getItem("objID"));
+let ID = JSON.parse(localStorage.getItem("objID"));
+let i = JSON.parse(localStorage.getItem("objIndex"));
 
-viewframe(obj,index);
+viewframe(obj);
 
-function viewframe(data,index) {
-    // event.preventDefault();
+function viewframe(data) {
     let title = document.querySelector("title");
     title.textContent = data.name;
 
@@ -32,13 +32,43 @@ function viewframe(data,index) {
 
    let cartBotton = document.querySelector("#cartBtn");
    cartBotton.addEventListener("click" , function() {
-    console.log(data);
+      addcart();
    });
 
    let info = document.querySelector("#informations");
    info.textContent = data.details;
 }
 
+function addcart() {
+
+    let currArr = JSON.parse(localStorage.getItem("currArr"));
+    let productIncart = JSON.parse(localStorage.getItem("newcart")) || [];
+
+    let cartdata = [];
+    cartdata.push(obj);
+    localStorage.setItem("prodList", JSON.stringify(cartdata));
+
+
+    if (productIncart.length == 0) {
+        currArr[i].qty = +1;
+        productIncart.push(currArr[i]);
+    } else {
+        let flag = false;
+        productIncart.map((e) => {
+            if (e.id == ID) {
+                e.qty = +e.qty + 1;
+                flag = true;
+            }
+        });
+
+        if (!flag) {
+            currArr[i].qty = +1;
+            productIncart.push(currArr[i]);
+        }
+    }
+
+    localStorage.setItem("newcart", JSON.stringify(productIncart));
+}
 
 
 
